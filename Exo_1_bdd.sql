d582789332d72a739dd7151f05c3540a324e2d4b
fadi-code <121521880+fadi-code@users.noreply.github.com>
2024-05-13 16:40:36 +0200

    suite exo1

--- Exo_1_bdd.sql
+++ Exo_1_bdd.sql
@@ -18,16 +18,12 @@ CREATE TABLE COM (
     Payement VARCHAR(50),
     FOREIGN KEY (NumCli) REFERENCES CLI(NumCli)
 );
-
--- Création de la table DET
-CREATE TABLE DET (
-    NumCom INT,
-    NumPro INT,
-    Qte INT,
-    Remise NUMERIC(5, 2),
-    FOREIGN KEY (NumCom) REFERENCES COM(NumCom),
-    FOREIGN KEY (NumPro) REFERENCES PRO(NumPro),
-    PRIMARY KEY (NumCom, NumPro)
+-- Création de la table FOU
+CREATE TABLE FOU (
+    NumFou INT PRIMARY KEY,
+    NomFou VARCHAR(255),
+    Pays VARCHAR(255),
+    Tel VARCHAR(20)
 );
 
 -- Création de la table PRO
@@ -40,10 +36,15 @@ CREATE TABLE PRO (
     FOREIGN KEY (NumFou) REFERENCES FOU(NumFou)
 );
 
--- Création de la table FOU
-CREATE TABLE FOU (
-    NumFou INT PRIMARY KEY,
-    NomFou VARCHAR(255),
-    Pays VARCHAR(255),
-    Tel VARCHAR(20)
+
+-- Création de la table DET
+CREATE TABLE DET (
+    NumCom INT,
+    NumPro INT,
+    Qte INT,
+    Remise NUMERIC(5, 2),
+    FOREIGN KEY (NumCom) REFERENCES COM(NumCom),
+    FOREIGN KEY (NumPro) REFERENCES PRO(NumPro),
+    PRIMARY KEY (NumCom, NumPro)
 );
+
